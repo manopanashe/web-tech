7a2f9ff2149938231702ce9c4a42093bbe5b679a
Panashe Mano <4manop68@solent.ac.uk>
2020-03-13 12:28:22 +0000

    commit

--- week-8/task.js
+++ week-8/task.js
@@ -2,6 +2,13 @@
 var random_number;
 var number_of_guess;
 function main(){
-
+random_number.round(random_number()* (max-min) + min);
   console.alert('working')  
+}
+
+function getRandomArbitrary(min,max){
+
+}
+function guess(){
+    number_of_guess = document.getElementById('number_of_guess').innerHTML;
 }
